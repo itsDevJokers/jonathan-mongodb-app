01b946aa9fdc95bb99a7c8b858563f98c115b8ab
itsDevJokers <jonathankarongkong97@gmail.com>
2022-03-06 19:12:49 +0800

    Membuat REST API dengan MongoDB dan Mongoose

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev')); // middleware untuk mengetahui aktivitas request
 
 app.use(cors()); // cors atau middleware untuk mengijinkan request data dari client
 app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
